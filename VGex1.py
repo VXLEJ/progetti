fd9698ba216d5b37760d7511afc555238dd81243
Valerio <117269806+VXLEJ@users.noreply.github.com>
2024-04-16 12:39:43 +0200

    Add files via upload

---
+++ VGex1.py
@@ -0,0 +1,4 @@
+Name = "Valerio"
+age = 20
+
+print("my name is {} and i am {} years old".format(Name,age))
---
+++ VGex2.py
@@ -0,0 +1,7 @@
+length = 20
+width = 10
+
+perimeter = (length*2)+(width*2)
+Area = length*width
+
+print("the perimeter is {} and the Area is {}".format(perimeter,Area))
